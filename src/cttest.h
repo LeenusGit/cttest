efec5946811bb97af287c9b511789f6f353abbab
LeenusGit <lkvikant@abo.fi>
2023-10-22 18:00:48 +0300

    Add unary checks

--- src/cttest.h
+++ src/cttest.h
@@ -3,6 +3,7 @@
 #include <ranges>
 #include <cstddef>
 #include <optional>
+#include <functional>
 
 #include <fmt/core.h>
 #include <fmt/compile.h>
@@ -12,22 +13,33 @@
 
 namespace ctt {
 
+struct true_t {
+    constexpr bool operator()(bool in) noexcept { return in == true; }
+};
+struct false_t {
+    constexpr bool operator()(bool in) noexcept { return in == false; }
+};
+
 template <typename Op>
 auto format_string = [] {
-    if constexpr (std::same_as<Op, std::ranges::equal_to>)  { return "expected: {} == {}"; }
-    if constexpr (std::same_as<Op, std::ranges::less>)      { return "expected: {} < {}"; }
+    if constexpr (std::same_as<Op, std::ranges::equal_to>)  { return "expected: {{{}}} == {{{}}}"; }
+    if constexpr (std::same_as<Op, std::ranges::less>)      { return "expected: {{{}}} < {{{}}}"; }
+    if constexpr (std::same_as<Op, true_t>)                 { return "expected: {{true}}, got: {{{}}}"; }
+    if constexpr (std::same_as<Op, false_t>)                { return "expected: {{{false}}}, got: {{{}}}"; }
     return "N/A";
 };
+
+using location = std::source_location;
 
 struct failure {
 
     constexpr failure (
         std::string_view reason,
-        std::source_location location = std::source_location::current()
+        location location = location::current()
     ) {
         auto end = fmt::format_to(
             buf.begin(),
-            FMT_COMPILE("\nFailed in file: {}:{} because: {}"),
+            FMT_COMPILE("\n\tin file:({}:{}), reason:\n\t\t{}"),
             location.file_name(),
             location.line(),
             reason
@@ -46,19 +58,28 @@ using result = std::optional<failure>;
 static constexpr auto passed = result{};
 
 template <typename Op>
-auto msg_to = [] (auto output, auto a, auto b) {
+static constexpr auto msg_to(auto output, auto a, auto b) {
     return fmt::format_to(
         output,
         FMT_COMPILE(format_string<Op>()),
         a, b
     );
-};
+}
+
+template <typename Op>
+static constexpr auto msg_to(auto output, auto a) {
+    return fmt::format_to(
+        output,
+        FMT_COMPILE(format_string<Op>()),
+        a
+    );
+}
 
 static constexpr auto expect(
     auto const& a,
     auto op,
     auto const& b,
-    const std::source_location location = std::source_location::current()
+    const location location = location::current()
     ) -> result {
 
     std::array<char, 64> data;
@@ -68,12 +89,29 @@ static constexpr auto expect(
     return op(a, b) ? std::nullopt : result{failure{reason, location}};
 }
 
-static constexpr auto expect_eq(auto a, auto b, const std::source_location location = std::source_location::current()) {
+static constexpr auto expect(
+    auto const& a,
+    auto op,
+    const location location = location::current()
+    ) -> result {
+
+    std::array<char, 64> data;
+    auto end = msg_to<decltype(op)>(data.begin(), a);
+    std::string_view reason{data.begin(), end};
+
+    return op(a) ? std::nullopt : result{failure{reason, location}};
+}
+
+static constexpr auto expect_eq(auto a, auto b, const location location = location::current()) {
     return expect(a, std::ranges::equal_to{}, b, location);
 }
 
-static constexpr auto expect_less(auto a, auto b, const std::source_location location = std::source_location::current()) {
+static constexpr auto expect_less(auto a, auto b, const location location = location::current()) {
     return expect(a, std::ranges::less{}, b, location);
+}
+
+static constexpr auto expect_true(auto a,const location location = location::current()) {
+    return expect(a, ctt::true_t{}, location);
 }
 
 static constexpr bool test (auto func) {
--- test/ut_simple.cpp
+++ test/ut_simple.cpp
@@ -4,23 +4,26 @@ int main() {
 
     int const rc = ctt::test(
         [] {
-
             int ans = 0;
-
             CTT_CHECK(ctt::expect_less(ans, 111));
             CTT_CHECK(ctt::expect_less(ans, 11));
             CTT_CHECK(ctt::expect_less(ans, 1));
 
             return ctt::passed;
         },
+        
         [] {
-
             int ans = 0;
-
             CTT_CHECK(ctt::expect_less(ans, 111));
             CTT_CHECK(ctt::expect_less(ans, 11));
             CTT_CHECK(ctt::expect_less(ans, 1));
 
+            return ctt::passed;
+        },
+
+        [] {
+            int ans = 42;
+            CTT_CHECK(ctt::expect_true(ans == 42));
             return ctt::passed;
         }
     
